36d2fa9ad640dbfad6303ab958421857b923a026
Rupika <rupika.d97@gmail.com>
2020-01-14 13:19:47 -0800

    refactored vaccuum and added main :D

---
+++ main.py
@@ -0,0 +1,18 @@
+import vacuumrobot as r
+
+
+def main():
+    # robot1 = r.Robot(10, 1, r.env1[10][1])
+    # r.dumb_robot(robot1, r.env1)
+
+    robot2 = r.Robot(10, 1, r.env1[10][1])
+    r.rand_robot(robot2, r.env1)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
--- vacuumrobot.py
+++ vacuumrobot.py
@@ -1,3 +1,17 @@
+import random as rand
+
+
+class Robot:
+    def __init__(self, x, y, location):
+        self.x = x
+        self.y = y
+        self.location = location
+        self.direction = "up"
+        self.power = "on"
+        self.dirt_sensor = 0
+        self.wall_sensor = 0
+        self.home_sensor = 0
+
 #First Pass Hard Code of env1
 env1 = [["Wall ", "Wall ","Wall ", "Wall ", "Wall ","Wall ","Wall ","Wall ","Wall ","Wall ","Wall ","Wall "],
         ["Wall ","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Wall "],
@@ -12,26 +26,9 @@ env1 = [["Wall ", "Wall ","Wall ", "Wall ", "Wall ","Wall ","Wall ","Wall ","Wal
         ["Wall ","Home ","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Dirty","Wall "],
         ["Wall ", "Wall ","Wall ","Wall ", "Wall ","Wall ","Wall ","Wall ","Wall ","Wall ","Wall ","Wall "]]
 
-#print(env1[10][1])
-
-class Robot:
-  def __init__(self, x, y, location, direction, power):
-    self.x = x
-    self.y = y
-    self.location = location
-    self.direction = direction
-    self.power = power
-    self.dirt_sensor = 0
-    self.wall_sensor = 0
-    self.home_sensor = 0
-
-robot1 = Robot(10, 1, env1[10][1], 'up', 'on')
-
-#print(robot1.location, robot1.direction)
-#print(robot1.x, robot1.y)
 
 def print_env():
-#print env1 out nicely
+    #print env1 out nicely
     for i in range(len(env1)):
         for j in range(len(env1[i])):
             print(env1[i][j], end=' ')
@@ -47,97 +44,123 @@ def number_clean():
     print("Number of spaces cleaned: ", clean_count)
 #print_env()
 
-def dumb_robot(robot1, env1):
-    #move_forward(robot1)
-    #suck(robot1, env1)
+def dumb_robot(robot, env1):
+    #move_forward(robot)
+    #suck(robot, env1)
     #print_env()
-    #turn_right(robot1)
-    #turn_left(robot1)
+    #turn_right(robot)
+    #turn_left(robot)
     actions = 0
-    while(robot1.power == 'on'):
-        if(robot1.dirt_sensor == 1):
+    while(robot.power == 'on'):
+        if(robot.dirt_sensor == 1):
             print('cleaning')
-            suck(robot1, env1)
-        elif(robot1.wall_sensor == 1):
+            suck(robot, env1)
+        elif(robot.wall_sensor == 1):
             print('turning right')
-            turn_right(robot1)
-            robot1.wall_sensor = 0
-        elif(robot1.home_sensor == 1):
+            turn_right(robot)
+            robot.wall_sensor = 0
+        elif(robot.home_sensor == 1):
             print('turning off')
-            robot1.power = 'off'
+            robot.power = 'off'
         else:
             print('moving forward')
-            move_forward(robot1)
+            move_forward(robot)
 
         actions+=1
-        check_sensors(robot1, env1)
+        check_sensors(robot, env1)
 
     print_env()
     print("Actions taken: ", actions)
     number_clean()
-        
-    
-def move_forward(robot1):
-    if(robot1.direction == 'up'):
+
+def rand_robot(robot, env1):
+    actions = 0
+    while(robot.power == 'on'):
+        if(robot.dirt_sensor == 1):
+            suck(robot, env1)
+            print('cleaning')
+        elif(robot.wall_sensor == 1):
+            turn = rand.choice(["left", "right"])
+            # turning left or right
+            turn_right_left(turn, robot)
+            robot.wall_sensor = 0
+        elif(robot.home_sensor == 1):
+            robot.power = 'off'
+            print('turning off')
+        else:
+            turn = rand.choice(["right", "forward"])
+            if turn != "forward":
+                turn_right_left(turn, robot)
+            else:
+                move_forward(robot)
+                print('moving forward')
+        actions+=1
+        check_sensors(robot, env1)
+
+    print_env()
+    print("Actions taken: ", actions)
+    number_clean()
+
+# robot takes left or right turn
+def turn_right_left(turn, robot):
+    if turn == "left":
+        turn_left(robot)
+        print('turning left')
+    else:
+        turn_right(robot)
+        print('turning right')
+
+
+def move_forward(robot):
+    if(robot.direction == 'up'):
         #print("moving up in the world")
-        robot1.x -=1
-    elif(robot1.direction == 'right'):
-        robot1.y +=1
-    elif(robot1.direction == 'down'):
-        robot1.x+=1
-    elif(robot1.direction == 'left'):
-        robot1.y-=1
-    robot1.location = env1[robot1.x][robot1.y]
+        robot.x -=1
+    elif(robot.direction == 'right'):
+        robot.y +=1
+    elif(robot.direction == 'down'):
+        robot.x+=1
+    elif(robot.direction == 'left'):
+        robot.y-=1
+    robot.location = env1[robot.x][robot.y]
 
-def suck(robot1, env1):
-    env1[robot1.x][robot1.y] = 'Clean'
-    robot1.dirt_sensor = 0
 
-def turn_right(robot1):
-    if(robot1.direction == 'up'):
-         robot1.direction = 'right'
-    elif(robot1.direction == 'right'):
-        robot1.direction = 'down'
-    elif(robot1.direction == 'down'):
-        robot1.direction = 'left'
-    elif(robot1.direction == 'left'):
-        robot1.direction = 'up'
-    print("now facing: ", robot1.direction)
+def suck(robot, env1):
+    env1[robot.x][robot.y] = 'Clean'
+    robot.dirt_sensor = 0
 
-def turn_left(robot1):
-    if(robot1.direction == 'up'):
-         robot1.direction = 'left'
-    elif(robot1.direction == 'right'):
-        robot1.direction = 'up'
-    elif(robot1.direction == 'down'):
-        robot1.direction = 'right'
-    elif(robot1.direction == 'left'):
-        robot1.direction = 'down'
-    print("now facing: ", robot1.direction)
 
-def check_sensors(robot1, env1):
-    if(robot1.location == 'Home '):
-        robot1.home_sensor = 1
-    if(env1[robot1.x][robot1.y] == 'Dirty'):
-        robot1.dirt_sensor = 1
-    if(robot1.direction == 'up' and env1[robot1.x-1][robot1.y] == 'Wall '):
-        robot1.wall_sensor = 1
-    elif(robot1.direction == 'down' and env1[robot1.x+1][robot1.y] == 'Wall '):
-        robot1.wall_sensor = 1
-    elif(robot1.direction == 'right' and env1[robot1.x][robot1.y+1] == 'Wall '):
-        robot1.wall_sensor = 1
-    elif(robot1.direction == 'left' and env1[robot1.x][robot1.y-1] == 'Wall '):
-        robot1.wall_sensor = 1
-        
-    
+def turn_right(robot):
+    if(robot.direction == 'up'):
+        robot.direction = 'right'
+    elif(robot.direction == 'right'):
+        robot.direction = 'down'
+    elif(robot.direction == 'down'):
+        robot.direction = 'left'
+    elif(robot.direction == 'left'):
+        robot.direction = 'up'
+    print("now facing: ", robot.direction)
 
-dumb_robot(robot1, env1)
+def turn_left(robot):
+    if(robot.direction == 'up'):
+        robot.direction = 'left'
+    elif(robot.direction == 'right'):
+        robot.direction = 'up'
+    elif(robot.direction == 'down'):
+        robot.direction = 'right'
+    elif(robot.direction == 'left'):
+        robot.direction = 'down'
+    print("now facing: ", robot.direction)
 
-#robot 1 pseudocode 
-    #Starts in bottom left
-    #if on dirty tile, clean tile
-    #if facing wall turn right
-    #if on home space, turn off
-#main things to figure out 1) how to get robot to clean inner rows (this will just sweep the outside), 2) should the robot turn off the first time it reaches home? are we allowed to have a home counter i.e., the 3rd time the robot reaches home it turns off, this would also prevent the robot turning off on its first move
-    
-    
+def check_sensors(robot, env1):
+    if(robot.location == 'Home '):
+        robot.home_sensor = 1
+    if(env1[robot.x][robot.y] == 'Dirty'):
+        robot.dirt_sensor = 1
+    if(robot.direction == 'up' and env1[robot.x-1][robot.y] == 'Wall '):
+        robot.wall_sensor = 1
+    elif(robot.direction == 'down' and env1[robot.x+1][robot.y] == 'Wall '):
+        robot.wall_sensor = 1
+    elif(robot.direction == 'right' and env1[robot.x][robot.y+1] == 'Wall '):
+        robot.wall_sensor = 1
+    elif(robot.direction == 'left' and env1[robot.x][robot.y-1] == 'Wall '):
+        robot.wall_sensor = 1
